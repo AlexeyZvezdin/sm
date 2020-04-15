7a452cbee7c08647e4227055138131958170d23f
Alexey Zvezdin <alexeyrush94@gmail.com>
2020-04-15 18:38:19 +0400

    prepared for deploy

--- pages/p/[id]/[comment].js
+++ pages/p/[id]/[comment].js
@@ -1,13 +1,13 @@
-import { useRouter } from 'next/router';
-// this seems not to work
-const Comment = (props) => {
-  const router = useRouter();
-  const { id, comment } = router.query;
-  return (
-    <>
-      <h4>Post: {id}</h4>
-      <p>Comment: {comment}</p>
-    </>
-  );
-};
-export default Comment;
+// import { useRouter } from 'next/router';
+// // this seems not to work
+// const Comment = (props) => {
+//   const router = useRouter();
+//   const { id, comment } = router.query;
+//   return (
+//     <>
+//       <h4>Post: {id}</h4>
+//       <p>Comment: {comment}</p>
+//     </>
+//   );
+// };
+// export default Comment;
