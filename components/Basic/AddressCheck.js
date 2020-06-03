2bcf22f952e995e75f2c840ad8371b9ca83a3cc9
vmarkin <viac.marin@gmail.com>
2020-06-03 14:37:02 -0400

    сделал красивое окошко нужно добавить адаптив

--- components/Basic/AddressCheck.js
+++ components/Basic/AddressCheck.js
@@ -1,5 +1,5 @@
-import { Component } from 'react'
-import './addressCheck.scss'
+import { Component } from 'react';
+import './addressCheck.scss';
 
 export default class AddressCheck extends Component {
   constructor(props) {
@@ -7,15 +7,23 @@ export default class AddressCheck extends Component {
   }
   render() {
     return (
-      <div className={'address-check__container'}>
-        <div onClick={() => {}} className={'address-check__window'}>
-          <h1>Проверим адрес?</h1>
-          <p>Мы хотим убедиться, что Ваш адрес входит в зону доставки.</p>
-          <button>Доставка</button>
-          <button>Заберу Сам</button>
-          <span>
-            После проверки вы вернетесь к выбору блюд
-            <img src="/img/ic-smile-emoji.svg" alt=":)" />
+      <div className={'address-check'}>
+        <div className={'address-check__window'}>
+          <h1 className={'address-check__title'}>Проверим адрес?</h1>
+          <p className={'address-check__text'}>
+            Мы хотим убедиться, что Ваш адрес входит в зону доставки.
+          </p>
+          <div className={'address-check__button-container'}>
+            <button className={'address-check__button-container__button address-check__button-container__button_mb'}>
+              Доставка
+            </button>
+            <button className={'address-check__button-container__button address-check__button-container__button_mb address-check__button-container__button_inverse'}>
+              Заберу Сам
+            </button>
+          </div>
+          <span className={'address-check__footer-text'}>
+            После проверки вы вернетесь к выбору блюд&nbsp;
+            <img src="/img/ic-smile-emoji.svg" alt=":)"  className={'address-check__icon'}/>
           </span>
         </div>
       </div>
